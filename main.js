f6ea152895a1e482a1b44e21ee514fc03e7efdd1
david hodgetts <david.demainlalune@gmail.com>
2011-07-25 17:09:20 +0200

    wip

--- main.js
+++ main.js
@@ -40,16 +40,19 @@ io.sockets.on('connection', function(socket){
   socket.emit('welcome', {id: sessionId});
 
   socket.on('user_new_path', function(msg){
-    socket.broadcast.emit('new_path', { point: msg.point });
+    socket.broadcast.emit('new_path', { point: msg.point, 
+                                        id: sessionId });
   });
 
 
   socket.on('user_add_point_to_path', function(msg){
-    socket.broadcast.emit('add_point_to_path',{ point: msg.point });
+    socket.broadcast.emit('add_point_to_path',{ point: msg.point,
+                                                id: sessionId });
   });
 
   socket.on('user_end_path', function(msg){
-    socket.broadcast.emit('end_path', { point: msg.point });
+    socket.broadcast.emit('end_path', { point: msg.point,
+                                        id: sessionId});
   });
 
   socket.on('disconnect', function(){
