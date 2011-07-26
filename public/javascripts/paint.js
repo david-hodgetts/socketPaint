75cdab1fe1f733da4fe4555308fcc2f504d4ea40
david hodgetts <david.demainlalune@gmail.com>
2011-07-26 11:22:37 +0200

    added hash_of_paths in paint.js

--- public/javascripts/paint.js
+++ public/javascripts/paint.js
@@ -1,15 +1,41 @@
 var socket = io.connect(), 
     id,
-    server_path;
+    server_path,
+    hash_of_paths = {};
 
 tool.minDistance = 10;
 tool.maxDistance = 45;
 
-var path;
+////////////////////////////////
+// util functions
 
 var toPoint = function(nodePoint){
   return new Point(nodePoint.x, nodePoint.y);
 }
+
+
+var getPath = function(remoteId){
+
+  if (! hash_of_paths.hasOwnProperty(remoteId)){
+    throw new Error("key not found in hash_of_paths");
+  }
+
+  return hash_of_paths[remoteId];
+}
+
+
+var newPath = function(remoteId) {
+  if (hash_of_paths.hasOwnProperty(remoteId)){
+    throw new Error("can't create new path -> key already exists");
+  }
+
+  hash_of_paths[remoteId] = new Path();
+
+  return hash_of_paths[remoteId];
+}
+
+//////////////////////
+// remote callbacks
 
 socket.on('welcome', function(data){
   id = data.id;
@@ -17,15 +43,26 @@ socket.on('welcome', function(data){
 });
 
 
+// NEW_PATH from remote
+
 socket.on('new_path', function(data){
-  server_path = new Path();
+
+  server_path = newPath(data.id);
+
   server_path.fillColor = new HSBColor(Math.random() * 360, 1, 1);
   server_path.add(data.point);
   
   console.log('new path ' + data.point.x);
 });
 
+
+
+// ADD_POINT_TO_PATH from remote
+
 socket.on('add_point_to_path', function(data){
+    
+  server_path = getPath(data.id); 
+
   var point = toPoint(data.point),
       secondPoint = point + 20;
   server_path.add(point);
@@ -34,14 +71,30 @@ socket.on('add_point_to_path', function(data){
   console.log('add point to path');
 });
 
+
+
+// END_PATH from remote
+
 socket.on('end_path', function(data){
+
+  server_path = getPath(data.id); 
+
   server_path.add(data.point);
   server_path.closed = true;
   server_path.smooth();
 
+
+  // remove path
+  delete hash_of_paths[data.id];
+
   console.log('end path');
 });
 
+
+///////////////////////
+// local drawing
+
+var localPath;
 
 function onFrame(event){
 // unused 
@@ -49,10 +102,10 @@ function onFrame(event){
 }
 
 function onMouseDown(event) {
-  path = new Path();
-  path.fillColor = new HSBColor(Math.random() * 360, 1, 1);
+  localPath = new Path();
+  localPath.fillColor = new HSBColor(Math.random() * 360, 1, 1);
 
-  path.add(event.point);
+  localPath.add(event.point);
 
   console.log(event);
 
@@ -66,17 +119,17 @@ function onMouseDrag(event) {
   var top = event.middlePoint + step;
   var bottom = event.middlePoint - step;
   
-  path.add(top);
-  path.insert(0, bottom);
-  path.smooth();
+  localPath.add(top);
+  localPath.insert(0, bottom);
+  localPath.smooth();
 
   socket.emit('user_add_point_to_path', {point: event.point});
 }
 
 function onMouseUp(event) {
-  path.add(event.point);
-  path.closed = true;
-  path.smooth();
+  localPath.add(event.point);
+  localPath.closed = true;
+  localPath.smooth();
 
   socket.emit('user_end_path', {point: event.point});
 }
